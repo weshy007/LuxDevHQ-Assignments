1f07d96271b2d4aff5b1a2abf200c9d73e3ab0ee
Weshy Joe <josephwaweru96@gmail.com>
2026-07-19 12:13:36 +0300

    Add SQL queries for filtering data in Section C

---
+++ sql-week2-assignment-waithaka-waweru/section_c_query.sql
@@ -0,0 +1,37 @@
+-- ============================================
+-- Section C: Querying the Data (Filtering with WHERE) 
+-- ============================================
+
+--Q15. Write a query to find all students who are in Form 4. 
+SELECT *
+FROM students
+WHERE class='Form 4';
+
+--Q16. Write a query to find all subjects in the Sciences department. 
+SELECT *
+FROM subjects
+WHERE department='Sciences';
+
+--Q17. Write a query to find all exam results where the marks are greater than or equal to 70. 
+SELECT *
+FROM exam_results
+WHERE marks >= 70;
+
+--Q18. Write a query to find all female students only. (Hint: gender = 'F') 
+SELECT *
+FROM students
+WHERE gender='F';
+
+
+--Q19. Write a query to find all students who are in Form 3 AND from Nairobi. 
+SELECT *
+FROM students
+WHERE class='Form 3'
+AND city='Nairobi';
+
+
+--Q20. Write a query to find all students who are in Form 2 OR Form 4. 
+SELECT *
+FROM students
+WHERE class='Form 2'
+OR class='Form 4';
